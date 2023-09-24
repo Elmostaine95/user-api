43c7da4307a0ee3f947eb18b889dba0adde90beb
Badr-Eddine El Mostaine <100159431+Elmostaine95@users.noreply.github.com>
2023-09-24 13:53:00 +0100

    add git workflow

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "postgres://postgres:admin@localhost:5432/user_api?sslmode=disable"
+	dsn := "postgres://postgres:adminadmin@my-database.cughk4hu7go7.eu-north-1.rds.amazonaws.com:5432/user_api"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database")
